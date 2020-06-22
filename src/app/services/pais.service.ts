06e50c2d6ba5a27a00d7654a55f79566d192f28f
Portm4n <ruben_sanfer_3@hotmail.com>
2020-06-22 13:29:55 +0200

    f

--- src/app/services/pais.service.ts
+++ src/app/services/pais.service.ts
@@ -8,13 +8,12 @@ import { map } from 'rxjs/operators';
 export class PaisService {
   constructor(private http: HttpClient) {}
   getPaises() {
-    return this.http.get('https://restcountries.eu/rest/v2/lang/es').pipe(
-      map((resp: any[]) => {
-        return resp.map((pais) => ({
-          nombre: pais.name,
-          codigo: pais.alpha3Code,
-        }));
-      })
-    );
+    return this.http
+      .get('https://restcountries.eu/rest/v2/lang/es')
+      .pipe(
+        map((resp: any[]) =>
+          resp.map((pais) => ({ nombre: pais.name, codigo: pais.alpha3Code }))
+        )
+      );
   }
 }
